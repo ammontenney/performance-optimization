181af397f9bb1d5c9020367dfad65c3ee95ecdcc
Ammon <ammon@tenney.biz>
2015-11-28 12:19:10 -0500

    Optimize index.html:     Minify images     Resize and decrease quality on pizzeria.jpg     Fix renderblocking CSS and JS     Minimized HTML and inline CSS/JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,42 @@
 var gulp = require('gulp');
-var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
-var imageresize = require('gulp-image-resize');
+var inline = require('gulp-inline');
+var uglify = require('gulp-uglify');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
 
-gulp.task('default', ['']);
+gulp.task('default', ['min_html']);
 
-gulp.task('min_images', function(){
-    min_img('img/**/*', 'dist/img');
-    min_img('views/images/**/*','dist/views/images');
+gulp.task('min_html', function(){
+    min_html_helper('*.html', 'dist');
+    min_html_helper('views/*.html', 'dist/views');
 });
 
-function min_img(src, dest){
+gulp.task('min_images', function(){
+    min_img_helper('img/**/*', 'dist/img');
+    min_img_helper('views/images/**/*','dist/views/images');
+});
+
+
+// Some helper functions for above
+
+function min_img_helper(src, dest){
     gulp.src(src)
         .pipe(imagemin())
         .pipe(gulp.dest(dest));
 }
 
-
+function min_html_helper(src,dest){
+    var opt = {
+        disabledTypes: ['svg', 'img', 'js'],
+        js: uglify,
+        css: minifyCSS
+        };
+    gulp.src(src)
+        .pipe(inline(opt))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(dest));
+}
 
 
 // end
